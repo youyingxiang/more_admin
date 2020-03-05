1817aadcf90e31c0f7f4aa553d5f736b8883db2e
yxx <1365831278@qq.com>
2020-03-05 14:30:43 +0800

    初始化后台

---
+++ app/Admin/Controllers/AgenController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Agent;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class AgenController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = '代理商';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Agent());
+        $grid->disableFilter();
+        $grid->column('id', __('Id'));
+        $grid->column('username', __('用户名'));
+        $grid->column('name', __('姓名'));
+        $grid->column('created_at', __('创建时间'));
+        $grid->column('updated_at', __('更新时间'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Agent::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('username', __('用户名'));
+        $show->field('name', __('姓名'));
+        $show->field('created_at', __('创建时间'));
+        $show->field('updated_at', __('更新时间'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Agent());
+
+        $form->text('username', __('用户名'))
+            ->creationRules(['required', "unique:agent"])
+            ->updateRules(['required', "unique:agent,username,{{id}}"]);
+        $form->text('name', __('姓名'))
+            ->rules('required');
+        $form->password('password', __('密码'))
+            ->rules('required|confirmed');
+        $form->password('password_confirmation', __('确认密码'))
+            ->rules('required')
+            ->default(function ($form) {
+                return $form->model()->password;
+            });
+        $form->ignore(['password_confirmation']);
+        $form->display('created_at', __('创建时间'));
+        $form->display('updated_at', __('更新时间'));
+        $form->saving(function (Form $form) {
+            if ($form->password && $form->model()->password != $form->password) {
+                $form->password = bcrypt($form->password);
+            }
+        });
+        return $form;
+    }
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->isLocal() && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            $this->app->register(TelescopeServiceProvider::class);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
